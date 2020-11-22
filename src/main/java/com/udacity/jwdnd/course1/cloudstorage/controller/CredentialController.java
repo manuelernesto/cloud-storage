22513d93488f0afb89deaced8cf2096548f06ac7
manuelernesto <manuelernesto@outlook.com>
2020-11-22 02:45:48 +0100

    #save credentials

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/CredentialController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/CredentialController.java
@@ -1,10 +1,40 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
+import com.udacity.jwdnd.course1.cloudstorage.model.Credential;
+import com.udacity.jwdnd.course1.cloudstorage.model.Note;
+import com.udacity.jwdnd.course1.cloudstorage.services.CredentialService;
+import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller()
 @RequestMapping()
 public class CredentialController {
 
+    private final UserService userService;
+    private final CredentialService credentialService;
+
+    public CredentialController(UserService userService, CredentialService credentialService) {
+        this.userService = userService;
+        this.credentialService = credentialService;
+    }
+
+
+    @PostMapping("/credential-save")
+    public String addCredential(Authentication auth,
+                                @ModelAttribute Credential credential,
+                                RedirectAttributes redirectAttributes) {
+
+        var user = userService.getUser(auth.getName());
+        credential.setUserid(user.getUserId());
+
+        credentialService.saveCredential(credential);
+
+        return "redirect:/home";
+    }
 }
--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/CredentialService.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/CredentialService.java
@@ -2,11 +2,13 @@ package com.udacity.jwdnd.course1.cloudstorage.services;
 
 import com.udacity.jwdnd.course1.cloudstorage.mapper.CredentialMapper;
 import com.udacity.jwdnd.course1.cloudstorage.model.Credential;
+import org.springframework.stereotype.Service;
 
 import java.security.SecureRandom;
 import java.util.Base64;
 import java.util.List;
 
+@Service
 public class CredentialService {
 
     private final HashService hashService;
