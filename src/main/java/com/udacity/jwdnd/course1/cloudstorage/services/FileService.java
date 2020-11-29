0bdfe401bc928c35c53382da29556a5ada798d19
manuelernesto <manuelernesto@outlook.com>
2020-11-29 11:53:06 +0100

    #add file service with save, getAllFiles and delete methods

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/FileService.java
@@ -0,0 +1,28 @@
+package com.udacity.jwdnd.course1.cloudstorage.services;
+
+import com.udacity.jwdnd.course1.cloudstorage.mapper.FileWrapper;
+import com.udacity.jwdnd.course1.cloudstorage.model.File;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FileService {
+ private final FileWrapper fileWrapper;
+
+    public FileService(FileWrapper fileWrapper) {
+        this.fileWrapper = fileWrapper;
+    }
+
+    public void save(File file){
+        fileWrapper.insert(file);
+    }
+
+    public void delete(Integer fileId){
+        fileWrapper.delete(fileId);
+    }
+
+    public List<File> getAllFiles(Integer userId){
+        return fileWrapper.getFiles(userId);
+    }
+}
